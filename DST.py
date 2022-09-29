2aa84fcf16cfef78853dcfe46b398bd764162b91
Allan <nouri2906@gmail.com>
2022-09-29 11:01:10 +0200

    test

--- DST.py
+++ DST.py
@@ -19,3 +19,25 @@ requests.post('https://api.statbank.dk/v1' + '/tableinfo', json=params).json()
 
 # Initialize the class with the target table
 metro = DstApi('METROX1')
+
+# Get the table summary
+metro.tablesummary(language='en')
+metro.variable_levels('SÆSON', language='en')
+
+dnkapk = DstApi('dnkapk')
+display(dnkapk.tablesummary(language='en'))
+display(dnkapk.variable_levels('INSTRUMENT', language='en'))
+
+params = {
+    'table': 'metrox1',
+    'format': 'BULK',
+    'variables': [
+        {'code': 'SÆSON', 'values': ['10']},
+        {'code': 'Tid', 'values': ['*']}
+    ]
+}
+
+r = requests.post('https://api.statbank.dk/v1' + '/data', json=params)
+
+df = pd.read_table(StringIO(r.text), sep=';')
+
