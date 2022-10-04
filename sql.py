7e6c26a465072554706d477de6fd3f064099b47f
Allan <60264273+allannouri@users.noreply.github.com>
2022-10-04 14:39:02 +0200

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+connection_string = "Driver={SQL Server}; Server=db-everest-prod; Database=C4DW; Trusted_Connection=yes"
+connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+engine = create_engine(connection_url)
+
+out = pd.read_sql("select * from db.schema.table", engine)
+exp = pd.read_sql_query("insert into db.schema.table values (X)", engine)
+
