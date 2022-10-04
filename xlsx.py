a4b4276a3f4f5d829ea6a2d50f4c6f19fa88b8c2
Allan <60264273+allannouri@users.noreply.github.com>
2022-10-04 14:46:38 +0200

    Update xlsx.py

--- xlsx.py
+++ xlsx.py
@@ -3,7 +3,7 @@ import win32com.client as win32
 # Open an existing workbook
 excel = win32.gencache.EnsureDispatch('Excel.Application')
 wb = excel.Workbooks.Open('workbook1.xlsx')
-wb = excel.Workbooks.Open("F:/Research/_Performing Credit Quantitative Research/R/scores_to_sql/Analysis.xlsx")
+wb = excel.Workbooks.Open("C:/workbook1.xlsx")
 wb.Save()
 wb.Close()
 excel.Visible = True
